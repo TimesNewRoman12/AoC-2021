ef4c4176a9113b77043d9ef5bac5cd49159f6e25
smironovics <smironovics@evolutiongaming.com>
2021-12-15 00:53:18 +0200

    day14 solution

--- src/main/scala/day14/MyApp.scala
+++ src/main/scala/day14/MyApp.scala
@@ -6,6 +6,7 @@ import day14.Utils._
 //https://adventofcode.com/2021/day/14
 object Solution {
 
+  // Map(PN -> B, OS -> K), etc
   def toPairsMap(input: List[String]): Map[String, String] = {
     input.foldLeft(Map.empty[String, String]) { case (acc, string) =>
       string.split(" -> ").toList match {
@@ -16,100 +17,72 @@ object Solution {
     }
   }
 
-  def part2(polymer: String, pairs: Map[String, String], steps: Int): Unit = {
-    // polymer producing a pair of polymers i.e. BB -> BNB -> Map(BN -> 1, NB -> 1)
+  def part2(polymer: String, pairs: Map[String, String], steps: Int): Long = {
+    // polymer producing a pair of polymers i.e. BB -> Map(BN -> 1, NB -> 1)
     val producedPairs: Map[String, Map[String, Long]] =
       pairs.foldLeft(Map.empty[String, Map[String, Long]]) {
         case (acc, (pair, _)) =>
-          val res = part1(pair, pairs, 1)
+          val produced = part1(pair, pairs, 1)
           val producedPairs: Map[String, Long] =
-            res.sliding(2).foldLeft(Map.empty[String, Long]) {
+            produced.sliding(2).foldLeft(Map.empty[String, Long]) {
               case (acc, pair) =>
                 val count = acc.getOrElse(pair, 0L)
                 acc + (pair -> (count + 1L))
             }
-          //println(s"$pair => $res")
-         // println(s"producedPairs => $producedPairs")
           acc + (pair -> producedPairs)
       }
 
-    //println(s"produceMap => $producedPairs")
-
-    def once(polymerAsMap: Map[String, Long]) =
-      polymerAsMap.foldLeft(Map.empty[String, Long]) { case (acc, (pair, ccount)) =>
-
-        val produces: Map[String, Long] = producedPairs.getOrElse(pair, Map.empty)
-        //val withCount = produces.view.mapValues(_ * count).toMap
-        val updatedMap = produces.foldLeft(acc) { case (acc, (pair, count)) =>
-          val alreadyProduced = acc.getOrElse(pair, 0L)
-          //println(s"inner pair => $pair")
-          //println(alreadyProduced)
-          //println(count)
-          //println(ccount)
-          acc + (pair -> (alreadyProduced + ccount))
-        }
-
-        //println(s"pair -> acc => $pair => $updatedMap")
-
-        updatedMap
-        /*
-        val produces: Map[String, Int] = producedPairs.getOrElse(pair, Map.empty)
-        val updatedMap = produces.foldLeft(acc) { case (acc, (pair, count)) =>
-          val alreadyProduced = acc.getOrElse(pair, 0)
-          acc + (pair -> (alreadyProduced + count))
-        }
-
-        updatedMap
-
-         */
+    def once(polymerAsMap: Map[String, Long]): Map[String, Long] =
+      polymerAsMap.foldLeft(Map.empty[String, Long]) {
+        case (acc, (pair, count)) =>
+          val produces: Map[String, Long] =
+            producedPairs.getOrElse(pair, Map.empty)
+          val updatedMap = produces.foldLeft(acc) { case (acc, (pair, _)) =>
+            val alreadyProduced = acc.getOrElse(pair, 0L)
+            acc + (pair -> (alreadyProduced + count))
+          }
+          updatedMap
       }
 
-    def polymerToMap(acc: String) =
+    def polymerToMap(acc: String): Map[String, Long] =
       acc.sliding(2).foldLeft(Map.empty[String, Long]) { case (acc, pair) =>
-        val produced =  acc.getOrElse(pair, 0L)
+        val produced = acc.getOrElse(pair, 0L)
         acc + (pair -> (produced + 1L))
       }
 
     val polymerAsMap = polymerToMap(polymer)
-    //println(polymerAsMap)
 
-    def loop(polymerAsMap: Map[String, Long], steps: Int) = {
+    def loop(polymerAsMap: Map[String, Long], steps: Int): Map[String, Long] = {
       (1 to steps).foldLeft(polymerAsMap) { case (acc, _) =>
-        val res = once(acc)
-        //println(s"qty: ${res.groupBy(identity).view.mapValues(_.length).toMap}")
-        res
+        once(acc)
       }
     }
 
     val result = loop(polymerAsMap, steps)
-    println(result)
+    val letters = pairs.values.toList.distinct
 
-    //val result: Map[String, Int] = once(polymer)
-    //println(result)
+    def countMatches(string: String, subset: String) =
+      string.length() - string.replaceAll(subset, "").length()
 
-    /*
-    def loop(acc: Map[String, Int], steps: Int) = {
-      (1 to steps).foldLeft(acc) { case (acc, _) =>
-        val res = once(acc)
-        //println(s"qty: ${res.groupBy(identity).view.mapValues(_.length).toMap}")
-        res
-      }
+    // HashMap(BH -> 2, BB -> 3, CB -> 1, HC -> 0) for B will return (2 + 3*2 + 1 + 0) = 7
+    val letterCounts: Seq[Long] = letters.map { letter =>
+      result.collect { case (key, value) =>
+        val repeats = countMatches(key, letter)
+        value * repeats
+      }.sum
     }
 
-     */
+    // each pair shares a two letter combination with the other except a last one
+    def removeSharedDuplicates(value: Long) =
+      (value.toDouble / 2).toLong + value % 2
 
-
+    removeSharedDuplicates(letterCounts.max) -
+      removeSharedDuplicates(letterCounts.min)
   }
 
   def part1(polymer: String, pairs: Map[String, String], steps: Int): String = {
-    def loop(acc: String) = {
-      (1 to steps).foldLeft(acc) { case (acc, _) =>
-        val res = once(acc)
-        println(res)
-        //println(s"qty: ${res.groupBy(identity).view.mapValues(_.length).toMap}")
-        res
-      }
-    }
+    def loop(acc: String): String =
+      (1 to steps).foldLeft(acc) { case (acc, _) => once(acc) }
 
     def once(acc: String) =
       acc.sliding(2).foldLeft("") { case (acc, pair) =>
@@ -117,37 +90,21 @@ object Solution {
         acc + pair.patch(1, in, 1)
       } + polymer.last
 
-    /*
-    val result = loop(polymer)
-    val countMap: Map[Char, Int] = result.groupBy(identity).view.mapValues(_.length).toMap
-
-    println(result)
-    println(result.length)
-
-    countMap.values.max - countMap.values.min
-     */
     loop(polymer)
   }
 
   val program = for {
-    //input <- readNumbers("day14/input.txt")
-    input <- readNumbers("day14/input_example.txt")
-    //input <- readNumbers("day14/test.txt")
+    input <- readNumbers("day14/input.txt")
+    //input <- readNumbers("day14/input_example.txt")
     polymer = input.head
     pairs = toPairsMap(input.drop(2))
 
+    part1String = part1(polymer, pairs, 10)
+    part1Counts = part1String.groupBy(identity).view.mapValues(_.length).toMap
+    part1Result = part1Counts.values.max - part1Counts.values.min
+    _ <- printAny { part1Result }
 
-    //_ <- printAny("NBCCNBBBCBHCB")
-
-   //_ <- printAny(part1(polymer, pairs, 5))
-   _ = part2(polymer, pairs, 40)
-
-
-
-    //_ <- printAny(part1(polymer, pairs, 4))
-    //_ = part2(polymer, pairs, 4)
-
-
+    _ <- printAny { part2(polymer, pairs, 40) }
   } yield ()
 }
 
--- src/main/scala/day14/test.sc
+++ src/main/scala/day14/test.sc
@@ -1,1 +1,10 @@
-val l = Math.ceil(91.toDouble / 2)
+val res: Long = 4384079139203L
+
+  (res.toDouble / 2).toLong + res % 2
+
+val s: String = "SD"
+
+def countMatches(string: String, subset: String) = string.length() - string.replaceAll(subset,"").length()
+
+countMatches("SS", "S")
+

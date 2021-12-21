bada6289fdc62f0d79a7c44d453d6d593eaf98fb
smironovics <smironovics@evolutiongaming.com>
2021-12-21 04:11:32 +0200

    day18 part1 done

--- src/main/scala/day18/MyApp.scala
+++ src/main/scala/day18/MyApp.scala
@@ -5,6 +5,7 @@ import cats.implicits.catsSyntaxOptionId
 import day18.Solution.SnailfishNumber.fromBuffer
 import day18.Utils._
 import helpers.Buffer
+import cats.implicits._
 
 //https://adventofcode.com/2021/day/18
 object Solution {
@@ -33,7 +34,7 @@ object Solution {
   def toBinaryString(value: Int) =
     String.format("%4s", value.toBinaryString).replace(' ', '0')
 
-  def fourthPairs = List(
+  def levels4 = List(
     "0000",
     "0001",
     "0010",
@@ -52,71 +53,160 @@ object Solution {
     "1111"
   )
 
+  def allLevels: Seq[String] = List("0", "00", "000", "0000", "0001", "001", "01", "010", "0010", "0011", "0100", "0101", "011", "0110", "0111",
+  "1", "10", "100", "1000", "1001", "101", "11", "110", "1010", "1011", "1100", "1101", "111", "1110", "1111")
+
   sealed trait SnailfishNumber {
-    //def reduce: SnailfishNumber
-    //def atDepth(n: Int): Option[SnailfishNumber]
+    def reduce: SnailfishNumber
     def get(depth: String): Option[SnailfishNumber]
     def findFirstToExplode: Option[(String, Pair)]
+    def magnitude: Int
   }
 
   case class RegularNumber(value: Int) extends SnailfishNumber {
     override def toString: String = value.toString
     def get(depth: String): Option[SnailfishNumber] = None
     def findFirstToExplode: Option[(String, Pair)] = None
-    //def atDepth(n: Int): Option[SnailfishNumber] = None
-    //def reduce: RegularNumber = this
+    def reduce: RegularNumber = this
+    def +(add: Int) = copy(value = value + add)
+    def magnitude = value
   }
   case class Pair(p1: SnailfishNumber, p2: SnailfishNumber)
       extends SnailfishNumber {
     override def toString = s"[${p1.toString}, ${p2.toString}]"
 
-    def findFirstToExplode: Option[(String, Pair)] = {
-      def firstRegularNumber(depth: String) = {
-        val (_, found) = depth
-          .foldLeft[(SnailfishNumber, Option[RegularNumber])]((this, None)) {
-            case ((acc, None), depth) =>
-              acc match {
-                case d: Pair =>
-                  depth match {
-                    case '0' => (d.p1, None)
-                    case '1' => (d.p2, None)
-                  }
-                case r: RegularNumber => (acc, r.some)
-              }
-            case ((acc, regularNumber), depth) => (acc, regularNumber)
-          }
-        found
-      }
+    def magnitude: Int = p1.magnitude * 3 + p2.magnitude * 2
 
-      val res = {
-        fourthPairs.map { depth => (depth, get(depth)) }.collectFirst {
-          case (depth, Some(pair: Pair)) => (depth, pair)
+    def +(value: SnailfishNumber) = Pair(
+      p1 = Pair(p1, p2),
+      p2 = value
+    )
+
+    def replace(depth: String, sfNumber: SnailfishNumber) = {
+      def loop(depth: List[Char], result: Pair): Pair = {
+        depth match {
+          case first :: Nil =>
+            first match {
+              case '0' => result.copy(p1 = sfNumber)
+              case '1' => result.copy(p2 = sfNumber)
+            }
+          case first :: tail =>
+            first match {
+              case '0' =>
+                result.p1 match {
+                  case p: Pair => result.copy(p1 = loop(tail, p))
+                  case _       => result
+                }
+              case '1' =>
+                result.p2 match {
+                  case p: Pair => result.copy(p2 = loop(tail, p))
+                  case _       => result
+                }
+            }
+          case Nil => result
         }
       }
-      val res2 = for {
-        (depth, pair) <- res
-        (left, right) = (
-          toBinaryString(Integer.parseInt(depth, 2) - 1),
-          toBinaryString(Integer.parseInt(depth, 2) + 1)
-        )
-        _ = println(s"left: $left, right: $right")
-        /*
-        replacement =
-          if (depth.drop(3) == "0") Pair(RegularNumber(0), pair.p2)
-          else Pair(pair.p1, RegularNumber(0))
-        rr = firstRegularNumber(right)
-        ll = firstRegularNumber(left)
-        _ = println(rr)
 
-         */
-      } yield ()
-
-
-
-      println(res2)
-
-      res
+      loop(depth.toList, this)
     }
+
+    def addToFirstRegular(left: Boolean, value: Int, depth: String) = {
+      def loop(depth: List[Char], result: Pair): Pair = {
+        depth match {
+          case first :: tail =>
+            first match {
+              case '0' =>
+                result.p1 match {
+                  case p: Pair          => result.copy(p1 = loop(tail, p))
+                  case r: RegularNumber => result.copy(p1 = r + value)
+                }
+              case '1' =>
+                result.p2 match {
+                  case p: Pair          => result.copy(p2 = loop(tail, p))
+                  case r: RegularNumber => result.copy(p2 = r + value)
+                }
+            }
+          case Nil =>
+            if (left)
+              result.p2 match {
+                case r: RegularNumber => result.copy(p2 = r + value)
+                // dig until we find first regular number
+                case p: Pair => result.copy(p2 = loop(List('0'), p))
+              }
+            else
+              result.p1 match {
+                case r: RegularNumber => result.copy(p1 = r + value)
+                // dig until we find first regular number
+                case p: Pair => result.copy(p1 = loop(List('1'), p))
+              }
+        }
+      }
+
+      println(s"get: ${get(depth)}")
+
+      loop(depth.toList, this)
+    }
+
+    def explode = (for {
+      (depth, pair) <- findFirstToExplode
+      (left, right) = (
+        toBinaryString(Integer.parseInt(depth, 2) - 1),
+        toBinaryString(Integer.parseInt(depth, 2) + 1)
+      )
+      _ = println(s"left: $left, right: $right")
+      replaced = replace(depth, RegularNumber(0))
+      leftR = pair.p1 match {
+        case RegularNumber(value) if depth != "0000" =>
+          replaced.addToFirstRegular(true, value, left)
+        case _ => replaced
+      }
+      rightR = pair.p2 match {
+        case RegularNumber(value) if depth != "1111" =>
+          leftR.addToFirstRegular(false, value, right)
+        case _ => leftR
+      }
+    } yield rightR).getOrElse(this)
+
+    def split = (for {
+      (depth, regNumber) <- findFirstToSplit
+    } yield replace(
+      depth,
+      Pair(
+        RegularNumber(Math.floor(regNumber.value.toDouble / 2).toInt),
+        RegularNumber(Math.ceil(regNumber.value.toDouble / 2).toInt)
+      )
+    )).getOrElse(this)
+
+    def reduce: Pair = {
+      def loop(pair: Pair): Pair = {
+        println(s"before exploded: $pair")
+        val exploded = pair.explode
+        println(s"exploded: $exploded")
+        if (exploded != pair) loop(exploded)
+        else {
+          println(s"before split: $pair")
+          val splitted = pair.split
+          println(s"after split: $splitted")
+          if (splitted != pair) loop(splitted)
+          else pair
+        }
+      }
+
+      loop(this)
+    }
+
+    def findFirstToExplode: Option[(String, Pair)] =
+      levels4.map { depth => (depth, get(depth)) }.collectFirst {
+        case (depth, Some(pair: Pair)) => (depth, pair)
+      }
+
+    def findFirstToSplit: Option[(String, RegularNumber)] =
+      allLevels
+        .map { depth => (depth, get(depth)) }
+        .collectFirst {
+          case (depth, Some(r: RegularNumber)) if r.value > 9 =>
+            (depth, r)
+        }
 
     def get(depth: String): Option[SnailfishNumber] =
       depth.foldLeft[Option[SnailfishNumber]](this.some) {
@@ -131,22 +221,36 @@ object Solution {
           }
         case (None, _) => None
       }
-    //def reduce = ???
   }
 
+  def numbers(buffers: List[Buffer]) = {
+    val r = for {
+      buffer <- buffers
+    } yield fromBuffer(buffer).map { case (_, n: Pair) => n }
+
+    r.sequence
+  }
+
+  def part1(numbers: List[SnailfishNumber]) =
+    numbers.reduce[SnailfishNumber] { case (n1: Pair, n2: Pair) => (n1 + n2).reduce }.magnitude
+
   val program = for {
-    //input <- readLines("day18/input.txt")
-    input <- readLines("day18/input_example.txt")
+    input <- readLines("day18/input.txt")
+    //input <- readLines("day18/input_example.txt")
     buffers = input.map(Buffer)
 
-    (buffer, number) <- fromBuffer(buffers.head)
+    sfNumbers <- numbers(buffers)
 
-    _ <- printAny { number }
+    _ <- printAny { part1(sfNumbers) }
+
     //_ <- printAny { number.get("0111") }
     // [[[[[9,8],1],2],3],4]
     //_ <- printAny { number.get("0000") }
     //_ <- printAny { number.get("0001") }
-    _ <- printAny { number.findFirstToExplode }
+    //_ <- printAny { number.findFirstToExplode }
+
+    //_ <- printAny { reduced.get("10") }
+    //_ <- printAny { reduced.split }
 
     // [[3, [2, [[7, 3], 1]]], [6, [5, [4, [3, 2]]]]]
     //_ <- printAny { number.get("0110") }
